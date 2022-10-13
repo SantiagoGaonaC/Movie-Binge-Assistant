68f1fdfcd65c5a77da22c010ef0663d4a0dd81ba
JossyrUpM <jossyr.pinto.2020@upb.edu.co>
2022-10-12 20:02:47 -0500

    Update

--- tasks/views.py
+++ tasks/views.py
@@ -126,37 +126,7 @@ def signin(request):
                 return render(request, 'signin.html', {"error": "Username or password is incorrect."})
         else:
             return render(request, 'signin.html', {"error": "Username not exists."})
-        
 
-@login_required
-def task_detail(request, task_id):
-    if request.method == 'GET':
-        task = get_object_or_404(Task, pk=task_id, user=request.user)
-        form = TaskForm(instance=task)
-        return render(request, 'task_detail.html', {'task': task, 'form': form})
-    else:
-        try:
-            task = get_object_or_404(Task, pk=task_id, user=request.user)
-            form = TaskForm(request.POST, instance=task)
-            form.save()
-            return redirect('tasks')
-        except ValueError:
-            return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error updating task.'})
-
-@login_required
-def complete_task(request, task_id):
-    task = get_object_or_404(Task, pk=task_id, user=request.user)
-    if request.method == 'POST':
-        task.datecompleted = timezone.now()
-        task.save()
-        return redirect('tasks')
-
-@login_required
-def delete_task(request, task_id):
-    task = get_object_or_404(Task, pk=task_id, user=request.user)
-    if request.method == 'POST':
-        task.delete()
-        return redirect('tasks')
     
 
 def password_reset_request(request):
