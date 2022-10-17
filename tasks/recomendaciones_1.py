689bdc1bb3f81701fbc6fbadafaa6e9fc82c30be
Omar Nova <omar.nova.2020@upb.edu.co>
2022-10-17 11:10:39 -0500

    Terminando (espero)

--- tasks/recomendaciones_1.py
+++ tasks/recomendaciones_1.py
@@ -1,35 +1,11 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import math
 
 
 def recomendacion(peliculas, user, peliGenres):
+ 
     movies_df = pd.DataFrame(peliculas)
 
-    """
-    #creamos una columna nueva que se llamará year
-    movies_df['year'] = movies_df.title.str.extract('(\(\d\d\d\d\))',expand=False)
-    #Quitando los paréntesis 
-    movies_df['year'] = movies_df.year.str.extract('(\d\d\d\d)',expand=False)
-    #Eliminar los años de la columna "título" y el espacio
-    movies_df['title'] = movies_df.title.str.replace(' (\(\d\d\d\d\))', '')
-    movies_df['genres'] = movies_df.genres.str.split('|')
-    """
 
-
-
-    """
-    movies_df_copy = peliGenres
-    for index, row in movies_df.iterrows():
-        for genre in row['genres']:
-
-            movies_df_copy.at[index,genre] = 1
-
-    print("movies_df_copy: :",movies_df_copy)
-
-    movies_df_copy = movies_df_copy.fillna(0)
-    """
     movies_df_copy = pd.DataFrame(peliGenres)
     inputMovies = pd.DataFrame(user)
 
@@ -39,7 +15,7 @@ def recomendacion(peliculas, user, peliGenres):
 
     #Luego fusionándolo para que podamos obtener el movieId. Lo está fusionando implícitamente por título. 
     inputMovies = pd.merge(inputId, inputMovies)
-
+    
 
 
     #Eliminar información que no usaremos del dataframe
@@ -53,14 +29,13 @@ def recomendacion(peliculas, user, peliGenres):
 
     userSubset = userSubset.reset_index(drop=True)
     tabla_generos = userSubset.drop("imdb_id",1).drop("title",1)
-    #tabla_generos = userSubset.drop("imdb_id",1).drop("title",1).drop("genres",1).drop("year",1).drop("_id",1).drop("id",1).drop("original_language",1).drop("original_title",1).drop("overview",1).drop("popularity",1).drop("production_companies",1).drop("budget",1).drop("production_countries",1).drop("release_date",1).drop("revenue",1).drop("runtime",1).drop("spoken_languages",1).drop("status",1).drop("tagline",1).drop("vote_average",1).drop("vote_count",1).drop("production_companies_number",1).drop("production_countries_number",1).drop("spoken_languages_number",1)
-
-    
-
+   
     perfil_usu = tabla_generos.transpose().dot(inputMovies['rating'])
+   
     generos = movies_df_copy.set_index(movies_df_copy['imdb_id'])
 
     generos = generos.drop("imdb_id",1).drop("title",1)
+   
     generos.shape
 
     recom = ((generos*perfil_usu).sum(axis=1))/(perfil_usu.sum())
@@ -71,9 +46,8 @@ def recomendacion(peliculas, user, peliGenres):
 
 
     #jsonTx = recommendation_df.to_json()
-
-
-    return recom.head(12).keys().tolist()
+ 
+    return recom.head(24).keys().tolist()
 
     
     
